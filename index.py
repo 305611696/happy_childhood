2fea49e8361c6fa0b7e171d41ade1bacdad1bf0a
wt <305611696@qq.com>
2019-01-14 13:27:18 +0800

    完成1.0

--- index.py
+++ index.py
@@ -16,18 +16,17 @@ class HChildHood(Bot):
     def __init__(self, request_data):
         super(HChildHood, self).__init__(request_data)
         self.pics = [
-            {'des': '小狗', 'pic': 'http://dbp-resource.gz.bcebos.com/217dd919-cd4f-3942-3594-11d05b9f5d1e/gou.png?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2019-01-10T03%3A02%3A15Z%2F-1%2F%2Fbcdb7178025288cb89ed463a9816b41b732e72a02311bd61f440e197845e8eed'},
-            {'des': '小浣熊', 'pic': 'http://dbp-resource.gz.bcebos.com/217dd919-cd4f-3942-3594-11d05b9f5d1e/chouyou.png?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2019-01-10T03%3A02%3A15Z%2F-1%2F%2F60b301f2588a21555cf23ced57309bf9c2064f6e014d7d6193d5908e68b5a24a'},
-            {'des': '小飞机', 'pic': 'http://dbp-resource.gz.bcebos.com/217dd919-cd4f-3942-3594-11d05b9f5d1e/feiji.png?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2019-01-10T03%3A02%3A15Z%2F-1%2F%2F8d9e91d2bd70d78e6469cd1896575c88fcd7f48e32ea7aa82b789c543231cf0a'},
-            {'des': '小猴子', 'pic': 'http://dbp-resource.gz.bcebos.com/217dd919-cd4f-3942-3594-11d05b9f5d1e/houzi.png?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2019-01-10T03%3A02%3A15Z%2F-1%2F%2Fdd4c2913f838a7917f6f5a4d964d0a69b22405b3a7c0983170fbc3c20e99b681'},
-            {'des': '小金鱼', 'pic': 'http://dbp-resource.gz.bcebos.com/217dd919-cd4f-3942-3594-11d05b9f5d1e/jinyu.png?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2019-01-10T03%3A02%3A15Z%2F-1%2F%2F2bcae622a12fa680f8d0f5dc58e741a339250a964a381671c5f6d7f46e609d37'},
-            {'des': '小猫咪', 'pic': 'http://dbp-resource.gz.bcebos.com/217dd919-cd4f-3942-3594-11d05b9f5d1e/mao.png?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2019-01-10T03%3A02%3A15Z%2F-1%2F%2F5ff1b80a13f0dd9ade9d0aeb98a1cbfddc4b18caab5b3ca6093b6688c9f36fa2'},
-            {'des': '猫头鹰', 'pic': 'http://dbp-resource.gz.bcebos.com/217dd919-cd4f-3942-3594-11d05b9f5d1e/maotouying.png?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2019-01-10T03%3A02%3A15Z%2F-1%2F%2F2f901f83649019a61d96e7757ed2ec60c5d1165a1fe1f4657cff3f53dad19ee5'},
-            {'des': '小青蛙', 'pic': 'http://dbp-resource.gz.bcebos.com/217dd919-cd4f-3942-3594-11d05b9f5d1e/qingwa%27.png?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2019-01-10T03%3A02%3A15Z%2F-1%2F%2F323211fff790feccb8831b12ba6df5eb175d5795b266c079a0961ecf14cfbc87'},
-            {'des': '小兔子', 'pic': 'http://dbp-resource.gz.bcebos.com/217dd919-cd4f-3942-3594-11d05b9f5d1e/tuzi.png?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2019-01-10T03%3A02%3A15Z%2F-1%2F%2F86dd636c2b87bf8689e18259fe64136c084adb98df0587a547b80e78e1a85731'},
-            {'des': '小鱼儿', 'pic': 'http://dbp-resource.gz.bcebos.com/217dd919-cd4f-3942-3594-11d05b9f5d1e/yu.png?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2019-01-10T03%3A02%3A15Z%2F-1%2F%2F9d56963b0cd710d506c4c56e252376f6ee3eebcc8a06f2b615fb79584a8e3082'},
-            {'des': '小章鱼', 'pic': 'http://dbp-resource.gz.bcebos.com/217dd919-cd4f-3942-3594-11d05b9f5d1e/zhangyu.png?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2019-01-10T03%3A02%3A15Z%2F-1%2F%2F228c93080f253336cbc5f9864e3a3798dd671ae7572fae679c37b2b6d048b8a3'},
-            {'des': '大猫咪', 'pic': 'http://dbp-resource.gz.bcebos.com/217dd919-cd4f-3942-3594-11d05b9f5d1e/maomi.png?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2019-01-10T03%3A02%3A15Z%2F-1%2F%2Fb58777285005d3f3208584bab3c31e8c309545e9db2b880f44d57c0594ef95c6'},
+            {'des': '小狗', 'pic': 'http://dbp-resource.gz.bcebos.com/217dd919-cd4f-3942-3594-11d05b9f5d1e/gou.png?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2019-01-14T03%3A30%3A38Z%2F-1%2F%2Fa2830cb8f4d2e71a5c7b9b6f86b68cb8206ec82a34c49f76e965d7201da0d61e'},
+            {'des': '小臭鼬', 'pic': 'http://dbp-resource.gz.bcebos.com/217dd919-cd4f-3942-3594-11d05b9f5d1e/chouyou.png?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2019-01-14T03%3A30%3A38Z%2F-1%2F%2F3ba24af94d380035d7c7ad2aa9cd6246702eac53944695c765383aba03ee63d5'},
+            {'des': '小飞机', 'pic': 'http://dbp-resource.gz.bcebos.com/217dd919-cd4f-3942-3594-11d05b9f5d1e/feiji.png?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2019-01-14T03%3A30%3A38Z%2F-1%2F%2F7e8b57490568a688ac3ff372cbe2ce2d3ae26398de68a5587d019dfe5c15ed64'},
+            {'des': '小猴子', 'pic': 'http://dbp-resource.gz.bcebos.com/217dd919-cd4f-3942-3594-11d05b9f5d1e/houzi.png?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2019-01-14T03%3A30%3A38Z%2F-1%2F%2Fbb6571b5a863dcbad1f5ee4d058bf503ec6e4b102fa9c2d7416100f5fac8a646'},
+            {'des': '小金鱼', 'pic': 'http://dbp-resource.gz.bcebos.com/217dd919-cd4f-3942-3594-11d05b9f5d1e/jinyu.png?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2019-01-14T03%3A30%3A38Z%2F-1%2F%2F51ec2f6465c5579476f5bb51edee92f89ec16f84f0224da0dca2a08908758548'},
+            {'des': '小猫咪', 'pic': 'http://dbp-resource.gz.bcebos.com/217dd919-cd4f-3942-3594-11d05b9f5d1e/mao.png?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2019-01-14T03%3A30%3A38Z%2F-1%2F%2F746c01ab873439ba78e2b67a61cc58b96537c1ef94bcb5b1ec7e1214b82e16da'},
+            {'des': '猫头鹰', 'pic': 'http://dbp-resource.gz.bcebos.com/217dd919-cd4f-3942-3594-11d05b9f5d1e/maotouying.png?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2019-01-14T03%3A30%3A38Z%2F-1%2F%2Fab546cb7f04a57b83664da26e70bf75b5efc094ef7da07a7ff81ac3ebee37ec0'},
+            {'des': '小青蛙', 'pic': 'http://dbp-resource.gz.bcebos.com/217dd919-cd4f-3942-3594-11d05b9f5d1e/qingwa%27.png?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2019-01-14T03%3A30%3A38Z%2F-1%2F%2Fe4e49e25611c11219473103a6244c414a8e45ff259e72f2fc2bc1815479544a1'},
+            {'des': '小兔子', 'pic': 'http://dbp-resource.gz.bcebos.com/217dd919-cd4f-3942-3594-11d05b9f5d1e/tuzi.png?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2019-01-14T03%3A30%3A38Z%2F-1%2F%2F4252b8422110445d354d530e894333c01352aecfeb7ff785a027c3060abc6b6e'},
+            {'des': '小鱼儿', 'pic': 'http://dbp-resource.gz.bcebos.com/217dd919-cd4f-3942-3594-11d05b9f5d1e/yu.png?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2019-01-14T03%3A30%3A38Z%2F-1%2F%2F9427c86d055832c88b7aa32576383a095172d21a91891e0fe941844a131f2a55'},
+            {'des': '小章鱼', 'pic': 'http://dbp-resource.gz.bcebos.com/217dd919-cd4f-3942-3594-11d05b9f5d1e/zhangyu.png?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2019-01-14T03%3A30%3A38Z%2F-1%2F%2F07f21dabf5b3b37b27b6e8af13a41de3dcd5f6b4d69cdfca4f404c7eec22dbd7'},
         ]
 
         self.add_launch_handler(self.launch_request)
@@ -43,7 +42,7 @@ class HChildHood(Bot):
         self.wait_answer()
         template = BodyTemplate1()
         template.set_title('快乐童年')
-        template.set_plain_text_content('欢迎进入快乐童年')
+        template.set_plain_text_content('欢迎进入快乐童年，可以尝试对我说开始画图。')
         template.set_background_image('https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532350870263&di=c93edb2fb9a3cfe7a632acc46cceba62&imgtype=0&src=http%3A%2F%2Ffile25.mafengwo.net%2FM00%2F0A%2FAC%2FwKgB4lMC26CAWsKoAALb5778DWg60.rbook_comment.w1024.jpeg')
         template.set_token('0c71de96-15d2-4e79-b97e-e52cec25c254')
         render_template = RenderTemplate(template)
